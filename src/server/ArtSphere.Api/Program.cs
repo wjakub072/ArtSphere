da138e3a6468247f226a000d6576962efb492c9f
Jakub Włodarczyk <wjakub072@gmail.com>
2023-03-16 21:31:02 +0100

    Basic program.cs setup, added config files

--- src/server/ArtSphere.Api/Program.cs
+++ src/server/ArtSphere.Api/Program.cs
@@ -1,20 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+using System.Globalization;
+using System.Security.Claims;
+using ArtSphere.Api;
+using ArtSphere.Api.Database;
+using ArtSphere.Api.Services;
+using ArtSphere.Models.Auth;
+using ArtSphere.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(formatProvider: CultureInfo.CurrentCulture)
+    .CreateBootstrapLogger();
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    
+    var builder = WebApplication.CreateBuilder(args);
+
+    // logging
+    builder.Host.UseSerilog((ctx, setup) => setup
+        .ReadFrom.Configuration(builder.Configuration));
+    
+    builder.Services.AddDbContext<ApplicationDatabaseContext>((config) => {
+        config.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDatabase"));
+        
+        if(builder.Environment.IsDevelopment())
+        {
+            config.EnableDetailedErrors();
+            config.EnableSensitiveDataLogging();
+        }
+    }, 
+    ServiceLifetime.Scoped, ServiceLifetime.Scoped);
+
+
+    builder.Services.AddControllers();
+
 
 app.UseHttpsRedirection();
 
@@ -22,4 +47,34 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen();
+    
+    var app = builder.Build();
+    
+    
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    
+    app.UseHttpsRedirection();
+    
+    app.UseAuthorization();
+    
+    app.MapControllers();
+
+    app.Run();
+    
+}
+catch (System.Exception ex)
+{
+    Log.Fatal(ex, ex.GetBaseException().Message);
+} 
+finally 
+{
+    Log.CloseAndFlush();
+}
